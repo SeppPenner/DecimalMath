e5445d60f232b9957dc1316d22073ca6704c635b
SeppPenner <HansM2013@gmx.de>
2021-01-20 20:27:23 +0100

    Fixed description, removed wrong license year.

--- src/DecimalMath/DecimalMath.cs
+++ src/DecimalMath/DecimalMath.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="DecimalMath.cs" company="Hämmer Electronics">
-//   Copyright (c) 2019 All rights reserved.
+//   The project is licensed under the MIT license.
 // </copyright>
 // <summary>
 //   Analogy of System.Math class for decimal types.
