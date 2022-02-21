a915e2d37ef42db81cda9c370b38b48d80909fc9
SeppPenner <HansM2013@gmx.de>
2022-02-21 16:34:07 +0100

    Updated build script.

--- PushPackage.bat
+++
@@ -1,3 +0,0 @@
-dotnet nuget push "src\DecimalMath\bin\Release\HaemmerElectronics.SeppPenner.DecimalMath.*.nupkg" -s "github" --skip-duplicate
-dotnet nuget push "src\DecimalMath\bin\Release\HaemmerElectronics.SeppPenner.DecimalMath.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
-PAUSE
